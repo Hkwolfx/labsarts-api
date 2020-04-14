4a65ab9da646c4de57d0cc9cec401b1890b823e4
Hkwolfx <bourlandalexis@orange.fr>
2020-04-14 18:57:44 +0200

    changing email form for SES production

--- helpers/email.js
+++ helpers/email.js
@@ -67,7 +67,7 @@ exports.linkPublishedParams = (email, data) => {
                     Charset: 'UTF-8',
                     Data: `
                         <html>
-                            <h1>New link published | reactnodeaws.com</h1>
+                            <h1>New link published | labs-arts.com</h1>
                             <p>A new link titled <b>${
                                 data.title
                             }</b> has been just publihsed in the following categories.</p>
